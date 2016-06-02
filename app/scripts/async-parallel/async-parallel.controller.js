1451f032b809801e85a27ba7454606966efb420e
Stavros Kounis <skounis@gmail.com>
2016-06-02 08:25:17 +0300

    feat(async) Demonstrate the parallel() method

--- app/scripts/async-parallel/async-parallel.controller.js
+++ app/scripts/async-parallel/async-parallel.controller.js
@@ -5,13 +5,51 @@
 		.module('supermodular.asyncparallel')
 		.controller('AsyncParallelController', AsyncParallelController);
 
-	AsyncParallelController.$inject = ['$scope', 'asyncParallelService'];
+	AsyncParallelController.$inject = ['$scope', '$timeout', 'asyncParallelService'];
 
 	/* @ngInject */
-	function AsyncParallelController($scope, asyncParallelService) {
+	function AsyncParallelController($scope, $timeout, asyncParallelService) {
 		var vm = angular.extend(this, {
-
+			run: run
 		});
+
+		/*
+		 * A complex taks that takes up to 5 seconds to be completed.
+		 */
+		function task1(callback){
+			console.log('Task 1 started. Wait for 5 sec for completion.');
+			$timeout(function(){
+				console.log('Task 1 completed.');
+				var errorOccurred = null;
+				callback(errorOccurred, 'one');
+			},
+			5000);
+		}
+
+		/*
+		 * A less complex taks that takes up to 2 seconds to be completed.
+		 */
+		function task2(callback){
+			console.log('Task 2 started. Wait for 2 sec for completion.');
+			$timeout(function(){
+				console.log('Task 2 completed.');
+				var errorOccurred = null;
+				callback(errorOccurred, 'two');
+			},
+			2000);
+		}
+
+		function run(){
+
+			async.parallel({
+				one: task1,
+				two: task2,
+				},
+				function(err, result){
+					console.log('Result: ', result);
+					console.log('DONE.');
+			});
+		}
 
 	}
 })();
