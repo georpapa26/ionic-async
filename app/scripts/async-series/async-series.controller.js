46e256398aecb2ccdabb62c9e0bbab5c35ef78ee
Stavros Kounis <skounis@gmail.com>
2016-06-02 08:18:09 +0300

    feat(async) Demonstrate the series() method

--- app/scripts/async-series/async-series.controller.js
+++ app/scripts/async-series/async-series.controller.js
@@ -16,10 +16,12 @@
 		/*
 		 * A complex taks that takes up to 5 seconds to be completed.
 		 */
-		function task1(){
+		function task1(callback){
 			console.log('Task 1 started. Wait for 5 sec for completion.');
 			$timeout(function(){
 				console.log('Task 1 completed.');
+				var errorOccurred = null;
+				callback(errorOccurred, 'one');
 			},
 			5000);
 		}
@@ -27,21 +29,26 @@
 		/*
 		 * A less complex taks that takes up to 2 seconds to be completed.
 		 */
-		function task2(){
+		function task2(callback){
 			console.log('Task 2 started. Wait for 2 sec for completion.');
 			$timeout(function(){
 				console.log('Task 2 completed.');
+				var errorOccurred = null;
+				callback(errorOccurred, 'two');
 			},
 			2000);
 		}
 
 		function run(){
-			console.log('Execute task 1.');
-			task1();
-			console.log('Execute task 2.');
-			task2();
 
-			console.log('DONE.');
+			async.series({
+				one: task1,
+				two: task2,
+				},
+				function(err, result){
+					console.log('Result: ', result);
+					console.log('DONE.');
+			});
 		}
 
 	}
